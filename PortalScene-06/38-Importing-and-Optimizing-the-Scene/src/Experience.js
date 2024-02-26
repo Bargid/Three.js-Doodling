1fe92ffac5654758c8f3971b0f7ee0270cdaee73
Louis Roby <lroby@redcross.ca>
2024-02-26 16:28:15 -0500

    Lesson 47, 48, 49 done

---
+++ PortalScene-06/38-Importing-and-Optimizing-the-Scene/src/Experience.js
@@ -0,0 +1,18 @@
+import { OrbitControls, useGLTF } from '@react-three/drei'
+
+export default function Experience()
+{
+
+    const model = useGLTF('./model/portal.glb')
+    console.log(model);
+
+    return <>
+
+        <color args={ ['#030202'] } attach="background" />
+
+        <OrbitControls makeDefault />
+
+        <mesh scale={ 1.5 }>
+        </mesh>
+    </>
+}
